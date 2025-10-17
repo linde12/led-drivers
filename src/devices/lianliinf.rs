3d5d47216140c991a4a224111879e2b9f8ac4f87
linde12 <lindeoscar@gmail.com>
2025-10-17 10:14:32 +0200

    refactor: flatten loop

--- src/devices/lianliinf.rs
+++ src/devices/lianliinf.rs
@@ -1,20 +1,20 @@
-use hidapi::{HidApi, HidResult};
+use hidapi::HidApi;
 
 /// LianLi-SL-infinity-v0.8
 const VENDOR_ID: u16 = 0x0cf2;
 const PRODUCT_ID: u16 = 0xa102;
 const CMD_MB_SYNC_ON: [u8; 4] = [0xe0, 0x10, 0x61, 0x01];
 
-pub fn setup() -> HidResult<()> {
+pub fn setup() -> anyhow::Result<()> {
     let api = HidApi::new()?;
 
-    for device in api.device_list() {
-        if device.vendor_id() == VENDOR_ID && device.product_id() == PRODUCT_ID {
-            // device found, turn on motherboard sync
-            let device = api.open(VENDOR_ID, PRODUCT_ID)?;
-            device.write(&CMD_MB_SYNC_ON)?;
-        }
-    }
+    api.device_list()
+        .find(|d| d.vendor_id() == VENDOR_ID && d.product_id() == PRODUCT_ID)
+        .ok_or_else(|| anyhow::anyhow!("LianLi SL Infinity device not found"))?;
+
+    // device found, turn on motherboard sync
+    let device = api.open(VENDOR_ID, PRODUCT_ID)?;
+    device.write(&CMD_MB_SYNC_ON)?;
 
     Ok(())
 }
